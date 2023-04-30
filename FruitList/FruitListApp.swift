f453886a310c85b960738e6a893b88db87dc530f
DodyDharma440 <dharma.dody@yahoo.com>
2023-04-30 23:06:55 +0800

    Initial Commit

---
+++ FruitList/FruitListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitListApp.swift
+//  FruitList
+//
+//  Created by Dodi Aditya on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
